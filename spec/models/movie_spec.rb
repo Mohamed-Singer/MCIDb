4bf6d45e5d68d28a523a6c914c5f8536f2e296e9
Mohamed-Singer <mohamed.singer@zyda.com>
2023-02-10 00:13:20 +0200

    add Movie model specs

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Movie, type: :model do
+  it { should validate_length_of(:title).is_at_most(250) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:release_date) }
+  it { should validate_presence_of(:featured) }
+end
