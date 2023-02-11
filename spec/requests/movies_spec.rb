988087b2b254090f36a2604ba165a08841af56ca
Mohamed-Singer <mohamed.singer@zyda.com>
2023-02-11 22:20:13 +0200

    adjust movies controller spec

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -3,23 +3,15 @@ require 'rails_helper'
 RSpec.describe "Movies", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/movies/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/movies/show"
+      get "/movies/"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /featured" do
     it "returns http success" do
-      get "/movies/featured"
+      get "/featuredMovies"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
